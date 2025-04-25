fc992dec4fcfb9a6ea484f8533de8eff3b4d384c
nickypangers <nixonpang98@gmail.com>
2025-04-26 00:16:19 +0800

    import confirm sheet

---
+++ BlockTimeLogger/Views/ImportLogbookConfirmView.swift
@@ -0,0 +1,18 @@
+//
+//  ImportLogbookConfirmView.swift
+//  BlockTimeLogger
+//
+//  Created by Nixon Pang on 25/4/2025.
+//
+
+import SwiftUI
+
+struct ImportLogbookConfirmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImportLogbookConfirmView()
+}
